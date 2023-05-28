228d388988b6c8387796f0080ea5b10878ee82cb
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2023-05-28 15:51:25 -0300

    criando perfil de desenvolvimento

---
+++ src/main/java/com/rodrigo/ProdManager/configuration/DevConfig.java
@@ -0,0 +1,30 @@
+package com.rodrigo.ProdManager.configuration;
+
+import com.rodrigo.ProdManager.services.DatabaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+    @Autowired
+    DatabaseService databaseService;
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiate() throws ParseException {
+
+        if(!"create".equals(strategy)){
+            return false;
+        }
+        databaseService.instantiateTestDatabase();
+
+        return true;
+    }
+}
